5e40b299f66f61aad6d1225aa941cc0fb71e6b77
nattegans <cibenelfarok@roc-dev.com>
2020-01-15 13:27:26 +0100

    h01

---
+++ h1/1.2.js
@@ -0,0 +1,3 @@
+for(i = 0; i < 10; i++) {
+    console.log("The value of i is: " + i);
+}
---
+++ h1/1.3.js
@@ -0,0 +1,7 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "mondeo",
+    aantalWielen: "4",
+    kleur: "Blauw",
+}
+console.log(mijnauto["kleur"]);
